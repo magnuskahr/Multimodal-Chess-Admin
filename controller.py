a6c953e6a9c5cf554c31d6804b6d1716d573b0fd
Magnus Jensen <magnuskahr@icloud.com>
2020-11-14 16:02:20 +0100

    lifted crash

--- controller.py
+++ controller.py
@@ -16,9 +16,9 @@ class Controller():
         return
 
     def board_lifted_square(self, square: Square):
-        piece = self.board.get(square)
-        print("lifting " + piece + " from " + square)
-        self._lift(piece, square)
+        _, color, piece = self.board.get(square)
+        print("lifting " + piece + " of color " + color + " from " + square)
+        self._lift(color, piece, square)
         return
 
     def board_placed_square(self, square: Square):
@@ -29,8 +29,9 @@ class Controller():
     def setForce(self, a, b):
         return
         
-    def _lift(self, piece, fromSquare):
+    def _lift(self, color, piece, fromSquare):
         self.lifting = {
+            "color": color,
             "piece": piece,
             "square": fromSquare
         }
